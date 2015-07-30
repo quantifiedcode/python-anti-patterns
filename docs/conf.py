142d3c37432f43b3b4d7c1f0ef3dbe75cdb4be85
Christoph Neumann <programmdesign@users.noreply.github.com>
2015-07-31 00:06:44 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python Anti-Patterns'
-copyright = u'2014, Andreas Dewes'
+copyright = u'2014-2015, QuantifiedCode'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
